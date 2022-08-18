f7f177993eab495c8896ec92a327441efe06b441
Andrei-Vladut CUTEANU <acuteanu@pentalog.com>
2022-08-18 10:09:42 +0300

    Fix for github actions

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,23 +1,28 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
     public WebDriver driver;
 
-    @Before
     public void Setup(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        chromeOptions.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://demo.automationtesting.in/Index.html");
         driver.manage().window().maximize();
     }
 
-    @After
     public void ClearEnvironment(){
         //driver.quit();
     }
